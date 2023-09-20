7c8cefbc8762e391c50c25de994fffccdb935a45
Samuel Lima <samuellimadossantosoficial@gmail.com>
2023-09-20 01:25:12 -0300

    home page desktop finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BlogTitleComponent } from './components/blog-title/blog-title.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainCardComponent,
+    SmallCardComponent,
+    BlogTitleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
